4bc80c3da0b4aa3fa083fa07d7bde986812fb233
kshivanku <kshivanku@gmail.com>
2017-02-19 21:15:04 -0500

    adding a loader

--- public/sketch.js
+++ public/sketch.js
@@ -6,6 +6,11 @@ function initialize() {
           url = $('input').val();
           //CANNOT SENT A URL IN A SIMPLE GET REQUEST SOMEHOW
           if(url){
+            $("#font_list").empty();
+            $("#color_blocks").empty();
+            $("#font_heading").html("");
+            $("#color_heading").html("");
+            $("#loading").css('display', 'block');
             var query = {
                 'url': url
             }
@@ -15,7 +20,7 @@ function initialize() {
               url: '/scrapecolor/',
               data: query,
               success: function(data){
-                  gotColors(data);
+                  gotData(data);
               }
             });
           }
@@ -23,13 +28,17 @@ function initialize() {
     });
 }
 
-function gotColors(data) {
-  // console.log(colors);
-  $("#color_blocks").empty();
-  $("h1").html("Total Colors: " + data.colors.length);
+function gotData(data) {
+  $("#loading").css("display", "none");
+  $("#font_heading").html("Total Fonts: " + data.fonts.length);
+  $("#color_heading").html("Total Colors: " + data.colors.length);
+  for(j = 0 ; j< data.fonts.length ; j++){
+    $("#font_list").append('<p id = font'+j+'></p>');
+    $("#font"+j).html(data.fonts[j]);
+  }
   for(i = 0; i < data.colors.length; i++){
-    $("#color_blocks").append('<div class="color_block" id='+i+'></div>');
-    $("#"+i).css('background-color', data.colors[i]);
-    $("#"+i).html(data.colors[i]);
+    $("#color_blocks").append('<div class="color_block" id=color'+i+'></div>');
+    $("#color"+i).css('background-color', data.colors[i]);
+    $("#color"+i).html(data.colors[i]);
   }
 }
